9ece1e9066ded7af3c2dbb53c025ddcac45aefe7
aesardi <83575924+aesardi@users.noreply.github.com>
2021-06-04 13:13:46 +0200

    Update run_Solea_senegalensis.m

--- run_Solea_senegalensis.m
+++ run_Solea_senegalensis.m
@@ -1,4 +1,4 @@
-%close all; 
+close all; 
 global pets
 
 pets = {'Solea_senegalensis'};
@@ -6,14 +6,16 @@ check_my_pet(pets);
 
 % Read about how to set estimation and output options (estim_options) on the online
 % manual: http://www.debtheory.org/wiki/index.php?title=Run_file
+% http://www.debtheory.org/wiki/index.php?title=Estimation_options#Loss_functions
 
 estim_options('default'); 
 estim_options('max_step_number',5e3); 
 estim_options('max_fun_evals',5e3);  
 
-estim_options('pars_init_method', 2); %1 to continue 2 read from pars_init
-estim_options('results_output', 1); % -1 in the command window 
-estim_options('method', 'nm');
+estim_options('pars_init_method', 1); %1 to continue 2 read from pars_init
+estim_options('results_output', 5); % -1 in the command window 
+estim_options('method', 'no');
+estim_options('loss_function', 'sb');
 estim_pars; 
 
 
